aea339da916b37c590d63b65f273be1075071e75
Faizan <faizanhuda12@gmail.com>
2020-05-16 13:03:39 -0400

    final

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("hi")
+
+AOS.init({
+  duration: 1400,
+})
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
